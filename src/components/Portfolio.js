2d4ce9d210711ccbc9742b09e197521c0200911b
Komyak <dynasoftstudios@gmail.com>
2026-01-03 21:16:30 +0100

    Update portfolio project images to new Discord CDN URLs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,7 +9,7 @@ const Portfolio = () => {
       title: 'Aura "Hatred"',
       description: 'Uses explicit contrast to the color red. The aura has a walk and idle animation. Used in "Elemental Forge"',
       tags: ['VFX', 'Aura', 'Animation'],
-      image: 'https://i.ibb.co/r2F88G0S/image.png',
+      image: 'https://cdn.discordapp.com/attachments/1329091508535558308/1457104269219205343/image.png?ex=695ac999&is=69597819&hm=de75578a7e2d95376e75e4779f6f664f41e013c67a8f0bf3f71afd4b90bc0db8&',
       link: 'https://youtu.be/FsRAMIQGW4s?si=EshZAHheq7GLGe_D&t=15'
     },
     {
@@ -17,7 +17,7 @@ const Portfolio = () => {
       title: 'Aura "Duality"',
       description: 'Fluid character idle and special VFX. Used in "Elemental Forge"',
       tags: ['Animation', 'Aura'],
-      image: 'https://i.ibb.co/vCJrTJHZ/image.png',
+      image: 'https://cdn.discordapp.com/attachments/1329091508535558308/1457104268871073832/image.png?ex=695ac999&is=69597819&hm=8a7baa3825769f4089fb740ea1df2da6daa2d0fac32fa233a4341e7ad465d5c3&',
       link: 'https://youtu.be/FsRAMIQGW4s?si=cOk3kQeOnpS7iWFa&t=4'
     },
     {
@@ -25,7 +25,7 @@ const Portfolio = () => {
       title: 'Aura "Dancer"',
       description: 'Breakdance idle animation together with a disco-esque color theme. Used in "Elemental Forge"',
       tags: ['VFX', 'Aura', 'Animation'],
-      image: 'https://i.ibb.co/rG4tV6XV/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103992244011048/image.png?ex=695ac957&is=695977d7&hm=45d3ed5a199026c13b5415592321de3e2e7b55da9f85a9295e533139c2c65bc4&=&format=webp&quality=lossless',
       link: 'https://youtu.be/FsRAMIQGW4s?si=FyGVQ3S_hNDjgH6I&t=29'
     },
     {
@@ -33,7 +33,7 @@ const Portfolio = () => {
       title: 'TD Tower VFX',
       description: 'Attack beams, damaged towers FX and general particles.',
       tags: ['VFX', 'Particles', 'Attacks'],
-      image: 'https://i.ibb.co/Kp72nKbt/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103992596201472/image.png?ex=695ac957&is=695977d7&hm=47af58632e23bd46bb08e9b6a0a58b6c5674c8c5dfb188c65636519aa262c0bb&=&format=webp&quality=lossless',
       link: 'https://youtu.be/_oiZ8L1Ma2o'
     },
    {
@@ -41,7 +41,7 @@ const Portfolio = () => {
       title: 'TD Unit VFX',
       description: 'Attacks and general particles.',
       tags: ['VFX', 'Particles', 'Attacks'],
-      image: 'https://i.ibb.co/DgSv3zmc/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103992990470187/image.png?ex=695ac957&is=695977d7&hm=bcfac746462b30823f19cf468ebd235ec166b98b3ad0dca0e81ff85a55da4359&=&format=webp&quality=lossless',
       link: 'https://youtu.be/_oiZ8L1Ma2o'
     },
    {
@@ -49,7 +49,7 @@ const Portfolio = () => {
       title: 'TD Unit Trails',
       description: 'Custom trails for units',
       tags: ['VFX', 'Trails'],
-      image: 'https://i.ibb.co/LDghvvxQ/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103993342918852/image.png?ex=695ac957&is=695977d7&hm=6ab740dd32accd7f173cb4213d401620951787b7394aad89e1a22c0dea7895ec&=&format=webp&quality=lossless',
       link: 'https://youtu.be/ncP24o4eyZM'
     },
     {
@@ -57,7 +57,7 @@ const Portfolio = () => {
       title: 'Combat FX and Anims',
       description: 'Clean VFX and trails that follow player fists, along animation that was used for a project including choppy animation',
       tags: ['VFX', 'Animations', 'Choppy'],
-      image: 'https://i.ibb.co/yns2dNTn/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103993695244429/image.png?ex=695ac957&is=695977d7&hm=9fd460ec99f457ae6b1e0592e953d8beae7bd2fc2889083fb6d321814d714021&=&format=webp&quality=lossless',
       link: 'https://youtu.be/kOhxsiKaF4w'
     },
     {
@@ -65,7 +65,7 @@ const Portfolio = () => {
       title: 'Collect VFX',
       description: 'Emitters that spew currency or add impact to an interaction.',
       tags: ['VFX', 'Currency', 'Trails', 'Impact'],
-      image: 'https://i.ibb.co/TqPkXX8V/image.png',
+      image: 'https://media.discordapp.net/attachments/1329091508535558308/1457103994215469333/image.png?ex=695ac957&is=695977d7&hm=23ab99ad0d043eceeaddd4ddbe73eb2ba8cf96914201c9920a95f3ec7f49b45b&=&format=webp&quality=lossless',
       link: 'https://youtu.be/ZKZfR6qrXMs'
     }
   ];
